b837001df609a1e72b555e0e52c0ef78f1579eb3
Kendy Nguyen <kendynguyen20@hotmail.com>
2018-01-31 16:02:43 +0200

    w3t2

--- src/app/services/media.service.ts
+++ src/app/services/media.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import {resolvePtr} from 'dns';
 
 @Injectable()
 export class MediaService {
+  Http: any;
 
-
+  status: string;
+  router: any;
   apiUrl = 'http://media.mw.metropolia.fi/wbma';
   username: string;
   password: string;
+  email: string;
 
 
   constructor(private http: HttpClient) { }
@@ -16,10 +19,12 @@ export class MediaService {
   public login() {
     console.log('uname: ' + this.username);
     console.log('pwd: ' + this.password);
+    console.log('mail: ' + this.email);
 
     const body = {
       username: this.username,
       password: this.password,
+      email: this.email,
     };
 
     const settings = {
@@ -28,8 +33,26 @@ export class MediaService {
 
     this.http.post(this.apiUrl + '/login', body, settings).subscribe(response => {
       console.log(response['token']);
+      localStorage.setItem('token', response['token']);
+      this.router.navigate('front');
+    }, (error: HttpErrorResponse) => {
+      console.log(error.error.message);
+      this.status = error.error.message;
     });
+  }
+
+  register(user) {
+    return this.http.post(this.apiUrl + '/users', user);
+  }
+
+    getUserData() {
+      const settings = {
+        headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token')),
+      };
+      return this.http.get(this.apiUrl + '/users/user', settings);
+    }
 
     }
-  }
+
+
 
